9d1f9ecb6c5ac670635d8f3077b3308ae268c1e0
LoganDeGruy <153325483+LoganDeGruy@users.noreply.github.com>
2024-01-05 14:32:01 -0600

    Create Cards.java

---
+++ Cards.java
@@ -0,0 +1,23 @@
+import java.*
+
+public class Cards {
+  String name;
+  int power;
+  int health;
+  int cost;
+  int posX;
+  int posY;
+
+  public Cards (String name, int power, int health, int cost, int posX, int posY) {
+    this.name = name;
+    this.power = power;
+    this.health = health;
+    this.cost = cost;
+    this.posX = posX;
+    this.posY = posY;
+  }
+
+  // other methods
+
+}
+    
